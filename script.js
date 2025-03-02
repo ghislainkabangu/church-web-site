d1d0aa4ad2077aa7d674713a9453ec75e2d8bffd
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2025-03-02 13:41:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let openMenu = document.querySelector(".openMenu");
 let navigation_mobile = document.querySelector(".navigation_mobile");
 let closeMenu = document.querySelector(".closeMenu");
-
+let link=document.querySelectorAll(".link")
 openMenu.addEventListener("click", () => {
   // openMenu.style = "display:none";
   navigation_mobile.classList.add("navigation_mobileJs");
@@ -12,3 +12,10 @@ closeMenu.addEventListener("click", () => {
   //openMenu.style = "display:block";
   navigation_mobile.classList.remove("navigation_mobileJs");
 });
+
+
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener("click",()=>{
+    navigation_mobile.classList.remove("navigation_mobileJs");
+  })
+}
